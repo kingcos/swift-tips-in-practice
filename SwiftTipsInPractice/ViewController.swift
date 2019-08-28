74a43a0433baf51ab980b4134b2515fd0f1f4f29
kingcos <2821836721v@gmail.com>
2019-08-29 01:08:57 +0800

    Support table view

--- SwiftTipsInPractice/ViewController.swift
+++ SwiftTipsInPractice/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    override func tableView(_ tableView: UITableView,
+                            didSelectRowAt indexPath: IndexPath) {
+        var vc: UIViewController
+        
+        switch indexPath.row {
+        case 0:
+            vc = Demo100ViewController()
+        case 1:
+            vc = Demo99ViewController()
+        default:
+            vc = UIViewController()
+        }
+        
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
 
